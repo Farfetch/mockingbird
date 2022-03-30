e26dcc2109b3cd0efe88efb252443c0cbdeebc69
Matheus Castelo Branco Queiroz <matheus.castelo@gmail.com>
2022-03-30 17:29:18 +0100

    Adding small documentation

--- src/Mockingbird/AppDelegate.swift
+++ src/Mockingbird/AppDelegate.swift
@@ -52,6 +52,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillResignActive(_ aNotification: Notification) {
     }
 
+    /// This function is called when the user selects "Check for updates" on the mockingbird menu
     @IBAction func checkForUpdates(_ sender: Any) {
         SUUpdater.shared().checkForUpdates(nil)
     }
